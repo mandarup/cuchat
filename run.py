54e7dc7458eed2817ac9e356d9b79263858edb5e
mandar upadhye <mandarupadhye@gmail.com>
2019-02-09 13:17:05 -0500

    pass port when running

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 #!/bin/env python
+import os
 import blackchat
 from blackchat import socketio
 
 app = blackchat.create_app()
 
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
